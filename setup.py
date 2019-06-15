c6aa42193dc38fab643dc015db49d2c9dbfcbbac
Protinon <lukasdeaton@gmail.com>
2019-06-14 23:57:25 -0400

    Added README to PyPI project

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from distutils.core import setup
 
+with open("README.md", "r") as ld:
+    long_description = ld.read()
+
 setup(
-  name='PyProbe',
-  packages=['pyprobe'],
-  version="0.1.0",
-  license='MIT',
-  description='Extract metadata from video files using ffprobe or mediainfo',
-  author='Protinon',
-  author_email='Protinon99@gmail.com',
-  url='https://github.com/Protinon/PyProbe',
-  keywords=['ffprobe', 'mediainfo', 'parser', 'video'],
-  install_requires=[],
-  python_requires='Python 3',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Natural Language :: English',
-    'Intended Audience :: Developers',
-    'Operating System :: OS Independent',
-    'License :: OSI Approved :: MIT License',
-    'Topic :: Multimedia :: Video',
-    'Topic :: Multimedia :: Sound/Audio :: Analysis',
-    'Programming Language :: Python :: 3 :: Only',
-  ],
+    name='PyProbe',
+    packages=['pyprobe'],
+    version="0.1.0",
+    license='MIT',
+    description='Extract metadata from video files using ffprobe or mediainfo',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Protinon',
+    author_email='Protinon99@gmail.com',
+    url='https://github.com/Protinon/PyProbe',
+    keywords=['ffprobe', 'mediainfo', 'parser', 'video'],
+    install_requires=[],
+    python_requires='Python 3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Programming Language :: Python :: 3 :: Only',
+],
 )
